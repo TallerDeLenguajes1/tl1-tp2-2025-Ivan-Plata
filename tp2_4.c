c77403596c87e19122c33fa92106e7b09a87e6ba
Ivan-Plata <ivanplataunt@gmail.com>
2025-04-08 19:35:20 -0300

    agrego funcion ListarPCs

--- tp2_4.c
+++ tp2_4.c
@@ -8,7 +8,10 @@ struct compu
     int anio;
     int cantidad_nucleos;
     char *tipo_cpu;
-} typedef compu;
+};
+typedef struct compu compu;
+
+void listarPCs(compu pcs[], int cantidad);
 
 int main()
 {
@@ -23,8 +26,22 @@ int main()
         pcs[i].velocidad = 1 + rand() % 3;
         pcs[i].anio = 2015 + rand() % 9;
         pcs[i].cantidad_nucleos = 1 + rand() % 8;
-        pcs[i].tipo_cpu = tipos[rand() + 6];
+        pcs[i].tipo_cpu = tipos[rand() % 6];
     }
 
+    listarPCs(pcs, 5);
+
     return 0;
+}
+
+void listarPCs(compu pcs[], int cantidad)
+{
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("--------------------Computadora %d------------------------\n", i + 1);
+        printf("Velocidad de procesamiento de %d GHz\n", pcs[i].velocidad);
+        printf("Fabricada en %d\n", pcs[i].anio);
+        printf("Cantidad de nucleos: %d\n", pcs[i].cantidad_nucleos);
+        printf("tipo de procesador: %s\n", pcs[i].tipo_cpu);
+    }
 }
