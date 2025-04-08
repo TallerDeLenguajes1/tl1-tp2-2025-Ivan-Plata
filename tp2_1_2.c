afd7496eb5d5aae053582bffe2d3181545cb5d02
Ivan-Plata <ivanplataunt@gmail.com>
2025-04-08 16:58:34 -0300

    Codigo modificado para aritmetica de punteros

---
+++ tp2_1_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 20
+
+int main()
+{
+    int i;
+    double vt[N];
+    double *p = vt;
+
+    srand(time(NULL));
+    for (i = 0; i < N; i++)
+    {
+        *(p + i) = 1 + rand() % 100;
+        printf("%f    ", *(p + i));
+    }
+    return 0;
+}
